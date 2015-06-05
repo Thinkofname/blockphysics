02127d710a1b449614463bac002512015a7aad76
Thinkofdeath <thinkofdeath@spigotmc.org>
2015-06-05 23:12:12 +0100

    Various changes +0.2

--- src/main/kotlin/uk/thinkofdeath/minecraft/physics/Plugin.kt
+++ src/main/kotlin/uk/thinkofdeath/minecraft/physics/Plugin.kt
@@ -6,7 +6,6 @@ import com.bulletphysics.collision.dispatch.CollisionFlags
 import com.bulletphysics.collision.dispatch.CollisionObject
 import com.bulletphysics.collision.dispatch.DefaultCollisionConfiguration
 import com.bulletphysics.collision.shapes.BoxShape
-import com.bulletphysics.collision.shapes.BvhTriangleMeshShape
 import com.bulletphysics.dynamics.DiscreteDynamicsWorld
 import com.bulletphysics.dynamics.RigidBody
 import com.bulletphysics.dynamics.RigidBodyConstructionInfo
@@ -20,17 +19,14 @@ import org.bukkit.entity.ArmorStand
 import org.bukkit.entity.Player
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityExplodeEvent
-import org.bukkit.event.player.PlayerChatEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
-import org.bukkit.inventory.ItemStack
-import org.bukkit.event.EventHandler as event
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.util.EulerAngle
-import java.util.*
 import java.util.concurrent.TimeUnit
 import javax.vecmath.Quat4f
 import javax.vecmath.Vector3f
+import org.bukkit.event.EventHandler as event
 
 class PhysicsPlugin : JavaPlugin(), Listener {
 
@@ -53,7 +49,7 @@ class PhysicsPlugin : JavaPlugin(), Listener {
     public override fun onEnable() {
         dynamicsWorld.setGravity(Vector3f(0f, -10f, 0f))
 
-        boxCollision.calculateLocalInertia(5f, boxInertia)
+        boxCollision.calculateLocalInertia(30f, boxInertia)
 
         getServer().getScheduler().runTaskTimer(this, { stepSimulation() }, 0, 1)
         getServer().getPluginManager().registerEvents(this, this)
@@ -63,18 +59,18 @@ class PhysicsPlugin : JavaPlugin(), Listener {
         blocks.forEach(PBlock::kill)
     }
 
-    event fun explode(e : EntityExplodeEvent) {
+    event fun explode(e: EntityExplodeEvent) {
         val loc = e.getLocation()
         blocks.forEach {
             val l = it.stand.getLocation()
-            if (loc.distanceSquared(l) < e.getYield()*e.getYield()) {
+            if (loc.distanceSquared(l) < e.getYield() * e.getYield() * e.getYield()) {
                 val vec = Vector3f(
                         (loc.getX() - l.getX()).toFloat(),
                         (loc.getY() - l.getY()).toFloat(),
                         (loc.getZ() - l.getZ()).toFloat()
                 )
                 vec.normalize()
-                vec.scale(-e.getYield() * 30)
+                vec.scale(-e.getYield() * 50)
                 it.body.setLinearVelocity(vec)
             }
         }
@@ -84,7 +80,7 @@ class PhysicsPlugin : JavaPlugin(), Listener {
             if (it.getType() == Material.TNT) {
                 continue
             }
-            val l = it.getLocation()
+            val l = it.getLocation().add(0.5,-0.5,0.5)
             val bl = PBlock(this, l, it.getState())
             blocks.add(bl)
             val vec = Vector3f(
@@ -93,14 +89,14 @@ class PhysicsPlugin : JavaPlugin(), Listener {
                     (loc.getZ() - l.getZ()).toFloat()
             )
             vec.normalize()
-            vec.scale(-e.getYield() * 30)
+            vec.scale(-e.getYield() * 50)
             bl.body.setLinearVelocity(vec)
             it.setType(Material.AIR)
             ite.remove()
         }
     }
 
-    event fun playerJoin(e : PlayerJoinEvent) {
+    event fun playerJoin(e: PlayerJoinEvent) {
         val loc = e.getPlayer().getLocation()
         val transform = Transform()
         transform.setIdentity()
@@ -117,7 +113,7 @@ class PhysicsPlugin : JavaPlugin(), Listener {
         players[e.getPlayer()] = body
     }
 
-    event fun playerLeave(e : PlayerQuitEvent) {
+    event fun playerLeave(e: PlayerQuitEvent) {
         dynamicsWorld.removeRigidBody(players.remove(e.getPlayer()))
     }
 
@@ -131,13 +127,13 @@ class PhysicsPlugin : JavaPlugin(), Listener {
             if (!it.body.isActive()) {
                 return@forEach
             }
-            for (y in -2 .. 2) {
-                for (z in -2 .. 2) {
-                    for (x in -2 .. 2) {
-                        val loc = it.location.clone().add(x.toDouble(),y.toDouble(),z.toDouble())
+            for (y in -2..2) {
+                for (z in -2..2) {
+                    for (x in -2..2) {
+                        val loc = it.location.clone().add(x.toDouble(), y.toDouble(), z.toDouble())
                         var b = loc.getBlock()
                         var bloc = b.getLocation()
-                        if (b.getType() == Material.AIR || visited.contains(bloc)) {
+                        if (b.getType() == Material.AIR || !b.getType().isOccluding() || visited.contains(bloc)) {
                             continue
                         }
                         visited.add(bloc)
@@ -151,26 +147,25 @@ class PhysicsPlugin : JavaPlugin(), Listener {
                         )
 
                         if (offset >= pool.size()) {
-                            val motionState = DefaultMotionState(transform)
-                            val info = RigidBodyConstructionInfo(0f, motionState, boxStaticCollision, Vector3f(0f, 0f, 0f))
+                            val info = RigidBodyConstructionInfo(0f, null, boxStaticCollision, Vector3f(0f, 0f, 0f))
                             val body = RigidBody(info)
                             body.setActivationState(0)
                             body.setCollisionFlags(body.getCollisionFlags() or CollisionFlags.STATIC_OBJECT)
-                            dynamicsWorld.addRigidBody(body)
-                            body.setActivationState(0)
                             pool.add(body)
                         }
 
                         val body = pool.get(offset)
                         body.setWorldTransform(transform)
-                        body.setActivationState(CollisionObject.ACTIVE_TAG)
+                        dynamicsWorld.removeRigidBody(body)
+                        dynamicsWorld.addRigidBody(body)
+                        body.setActivationState(0)
 
                         offset++
                     }
                 }
             }
         }
-        for (i in offset .. pool.size() - 1) {
+        for (i in offset..pool.size() - 1) {
             pool[i].setActivationState(CollisionObject.DISABLE_SIMULATION)
         }
 
@@ -183,32 +178,35 @@ class PhysicsPlugin : JavaPlugin(), Listener {
             body.getCenterOfMassTransform(t)
             t.origin.set(
                     loc.getX().toFloat(),
-                    loc.getY().toFloat()+ 0.9f,
+                    loc.getY().toFloat() + 0.9f,
                     loc.getZ().toFloat()
             )
             body.setCenterOfMassTransform(t)
             body.setActivationState(CollisionObject.ACTIVE_TAG)
-            dynamicsWorld.debugDrawWorld()
         }
 
         val now = System.nanoTime()
-        dynamicsWorld.stepSimulation((now - lastSim).toFloat() / TimeUnit.SECONDS.toNanos(1).toFloat())
+        val delta = (now - lastSim).toFloat() / TimeUnit.SECONDS.toNanos(1).toFloat()
+        dynamicsWorld.stepSimulation(delta, 100)
         lastSim = now
-        blocks.forEach(PBlock::tick)
-        blocks.forEach({ if (!it.body.isActive()) {
-            it.kill()
-        } })
+        blocks.forEach({ it.tick(delta) })
+        blocks.forEach({
+            if (!it.body.isActive() || it.stand.isDead()) {
+                it.kill()
+            }
+        })
         blocks.removeIf({ !it.body.isActive() || it.stand.isDead() })
         visited.clear()
     }
 }
 
-class PBlock(val plugin : PhysicsPlugin, val location : Location, val block : BlockState) {
+class PBlock(val plugin: PhysicsPlugin, val location: Location, val block: BlockState) {
 
-    val stand : ArmorStand
-    val body : RigidBody
+    val stand: ArmorStand
+    val body: RigidBody
     val transform = Transform()
     val drops = block.getBlock().getDrops();
+    var life = 60f
 
     init {
         location.setYaw(0f)
@@ -226,36 +224,35 @@ class PBlock(val plugin : PhysicsPlugin, val location : Location, val block : Bl
         )
 
         val motionState = DefaultMotionState(transform)
-        val info = RigidBodyConstructionInfo(5f, motionState, plugin.boxCollision, plugin.boxInertia)
+        val info = RigidBodyConstructionInfo(30f, motionState, plugin.boxCollision, plugin.boxInertia)
+        info.additionalDamping = true
         body = RigidBody(info)
         plugin.dynamicsWorld.addRigidBody(body)
     }
 
-    fun tick() {
+    fun tick(delta: Float) {
         body.getMotionState().getWorldTransform(transform)
 
         var rot = Quat4f()
         transform.getRotation(rot)
-        rot.y += Math.PI.toFloat()
-        stand.setHeadPose(EulerAngle(
-                rot.x.toDouble(),
-                rot.y.toDouble(),
-                rot.z.toDouble()
-        ))
+        val eul = quatToEul(rot)
+        stand.setHeadPose(eul)
+
 
 
         location.setX(transform.origin.x.toDouble())
-        location.setY(transform.origin.y.toDouble() - 1.8f + 0.1f)
+        location.setY(transform.origin.y.toDouble() - 1.8f + 0.05f)
         location.setZ(transform.origin.z.toDouble())
 
         location.add(
-                -Math.sin(rot.z.toDouble()) * 0.15,
-                -Math.cos(rot.x.toDouble()) * 0.15 -Math.cos(rot.z.toDouble()) * 0.15,
-                -Math.sin(rot.x.toDouble()) * 0.15
+                -Math.sin(eul.getZ().toDouble()) * 0.15,
+                -Math.cos(eul.getX().toDouble()) * 0.15 - Math.cos(eul.getZ().toDouble()) * 0.15,
+                -Math.sin(eul.getX().toDouble()) * 0.15
         )
 
-        if (location.getY() < - 10 || location.getY() > 300 || stand.isDead()) {
-            kill()
+        life -= delta
+        if (location.getY() < -10 || location.getY() > 300 || stand.isDead() || life < 0) {
+            stand.remove()
         }
         stand.teleport(location)
     }
@@ -263,10 +260,44 @@ class PBlock(val plugin : PhysicsPlugin, val location : Location, val block : Bl
     fun kill() {
         stand.remove()
         plugin.dynamicsWorld.removeRigidBody(body)
-        stand.remove()
         val loc = stand.getLocation()
         drops.forEach {
             loc.getWorld().dropItemNaturally(loc, it)
         }
     }
+}
+
+fun quatToEul(q: Quat4f): EulerAngle {
+    val sqw = q.w * q.w
+    val sqx = q.x * q.x
+    val sqy = q.y * q.y
+    val sqz = q.z * q.z
+    val unit = sqx + sqy + sqz + sqw
+    val test = q.x * q.y + q.z * q.w
+    if (test > 0.499 * unit) {
+        return EulerAngle(
+                Math.PI / 2,
+                2 * Math.atan2(q.x.toDouble(), q.w.toDouble()),
+                0.0
+        )
+    }
+    if (test < -0.499 * unit) {
+        return EulerAngle(
+                -Math.PI / 2,
+                -2 * Math.atan2(q.x.toDouble(), q.w.toDouble()),
+                0.0
+        )
+    }
+
+    return EulerAngle(
+            Math.atan2(
+                    (2 * q.y * q.w - 2 * q.x * q.z).toDouble(),
+                    (sqx - sqy - sqz + sqw).toDouble()
+            ),
+            -Math.atan2(
+                    (2 * q.x * q.w - 2 * q.y * q.z).toDouble(),
+                    (-sqx + sqy - sqz + sqw).toDouble()
+            ),
+            -Math.asin((2 * test / unit).toDouble())
+    )
 }
